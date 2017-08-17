47396b164997c4c74394c44bf849e79798824784
王嘉晨 <wangjiachen@uenit.cn>
2017-08-17 17:24:32 +0800

    更新配置，开始逻辑部分

---
+++ src/components/progress/Progress.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data () {
+      return {}
+    },
+    mounted () {
+    },
+    props: {},
+    methods: {},
+    components: {}
+  }
+</script>
+
+<style scoped lang="scss" type="text/scss" rel="stylesheet/scss">
+    @import "~common/scss/variable";
+    @import "~common/scss/mixin";
+</style>
